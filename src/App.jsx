2cffc067ca6b990b0314b748e094dd41146c782d
Yair Fernando Bossio Chamorro <ybossiochamorro@gmail.com>
2026-07-09 17:23:34 -0500

    cambiando clases a className, pasando props y recorriendo data base

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,30 @@
 
 import './App.css'
 import Guitar from './components/Guitar'
+import { useState } from "react"
+import { db } from "./data/db"
+
 
 function App() {
-  
+   const [data, setData] = useState(db)
+
+console.log(data)
 
   return (
     <>
       
 
-    <main class="container-xl mt-5">
-        <h2 class="text-center">Nuestra Colección</h2>
+    <main className="container-xl mt-5">
+        <h2 className="text-center">Nuestra Colección</h2>
 
-        <div class="row mt-5">
-           <Guitar/>
+        <div className="row mt-5">
+          {
+            data.map((guitar) =>{
+             return <Guitar 
+             key={guitar.id}
+             guitar={guitar}/>
+            })
+          }
          
 
            
@@ -21,9 +32,9 @@ function App() {
     </main>
 
 
-    <footer class="bg-dark mt-5 py-5">
-        <div class="container-xl">
-            <p class="text-white text-center fs-4 mt-4 m-md-0">GuitarLA - Todos los derechos Reservados</p>
+    <footer className="bg-dark mt-5 py-5">
+        <div className="container-xl">
+            <p className="text-white text-center fs-4 mt-4 m-md-0">GuitarLA - Todos los derechos Reservados</p>
         </div>
     </footer>
     </>
--- src/components/Guitar.jsx
+++ src/components/Guitar.jsx
@@ -1,21 +1,19 @@
-import { useState } from "react"
 
-export default function Guitar(){
+export default function Guitar({guitar}){
 
-  const [auth, setAuth] = useState(false)
-console.log(auth)
+ 
     return(
-         <div class="col-md-6 col-lg-4 my-4 row align-items-center">
-                <div class="col-4">
-                    <img class="img-fluid" src="./public/img/guitarra_01.jpg" alt="imagen guitarra" />
+         <div id={guitar.id} className="col-md-6 col-lg-4 my-4 row align-items-center">
+                <div className="col-4">
+                    <img className="img-fluid" src={`/img/${guitar.image}.jpg`} alt="imagen guitarra" />
                 </div>
-                <div class="col-8">
-                    <h3 class="text-black fs-4 fw-bold text-uppercase">Lukather</h3>
-                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit quae labore odit magnam in autem nesciunt, amet deserunt</p>
-                    <p class="fw-black text-primary fs-3">$299</p>
+                <div className="col-8">
+                    <h3 className="text-black fs-4 fw-bold text-uppercase">{guitar.name}</h3>
+                    <p>{guitar.description}</p>
+                    <p className="fw-black text-primary fs-3">{guitar.price}</p>
                     <button 
                         type="button"
-                        class="btn btn-dark w-100"
+                        className="btn btn-dark w-100"
                     >Agregar al Carrito</button>
                 </div>
             </div>
