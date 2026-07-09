d6fbb58968ccf86ee3e2ef382c54b4191a7bef0d
Yair Fernando Bossio Chamorro <ybossiochamorro@gmail.com>
2026-07-09 17:46:56 -0500

    Agragando función a componente y utilizando destructuración para obtener props

--- src/components/Guitar.jsx
+++ src/components/Guitar.jsx
@@ -1,20 +1,23 @@
 
 export default function Guitar({guitar}){
-
- 
+   const {id, image, name, description , price} = guitar
+   const handleClick = (id) => {
+    console.log("Diste click"+" " + id)
+   }
     return(
-         <div id={guitar.id} className="col-md-6 col-lg-4 my-4 row align-items-center">
+         <div className="col-md-6 col-lg-4 my-4 row align-items-center">
                 <div className="col-4">
-                    <img className="img-fluid" src={`/img/${guitar.image}.jpg`} alt="imagen guitarra" />
+                    <img className="img-fluid" src={`/img/${image}.jpg`} alt="imagen guitarra" />
                 </div>
                 <div className="col-8">
-                    <h3 className="text-black fs-4 fw-bold text-uppercase">{guitar.name}</h3>
-                    <p>{guitar.description}</p>
-                    <p className="fw-black text-primary fs-3">{guitar.price}</p>
+                    <h3 className="text-black fs-4 fw-bold text-uppercase">{name}</h3>
+                    <p>{description}</p>
+                    <p className="fw-black text-primary fs-3">{price}</p>
                     <button 
                         type="button"
                         className="btn btn-dark w-100"
-                    >Agregar al Carrito</button>
+                        onClick={() => handleClick(id)}
+                    >Agregar al Carrito </button>
                 </div>
             </div>
     )
